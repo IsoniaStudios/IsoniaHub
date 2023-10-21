23cba8e4cb8539532d19a425dd0daac93ec13a9c
Simon <Zhyosgames@gmail.com>
2023-10-21 12:37:42 +0200

    window chrome maximize fixes

--- IsoniaHub/HubViewModel.cs
+++ IsoniaHub/HubViewModel.cs
@@ -1,16 +1,16 @@
-﻿using IsoniaCore.ViewModels;
-using System;
+﻿using IsoniaCore.DataTypes;
 using System.Reflection;
+using System;
 
 namespace IsoniaHub;
 
-public class HubViewModel : ViewModelBase
+public class HubViewModel : Observable
 {
     private static readonly Version? version = Assembly.GetEntryAssembly()?.GetName().Version;
     private static readonly string? title = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyTitleAttribute>()?.Title;
 
     public Version? Version => version;
-    public new string? Title => title;
+    public string? Title => title;
 
     public HubViewModel()
     {
